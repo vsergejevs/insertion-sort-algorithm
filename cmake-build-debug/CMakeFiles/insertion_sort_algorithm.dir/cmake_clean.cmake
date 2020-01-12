cdc0006522ecdc7bdc8831e7ec1f0eb81784246d
vsergejevs <valeriy.sergeyev7@gmail.com>
2020-01-12 20:41:07 +0000

    Added random integer generator. Printing to screen array with 10 random numbers

---
+++ cmake-build-debug/CMakeFiles/insertion_sort_algorithm.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/insertion_sort_algorithm.dir/main.cpp.o"
+  "insertion_sort_algorithm"
+  "insertion_sort_algorithm.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/insertion_sort_algorithm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- main.cpp
+++ main.cpp
@@ -1,9 +1,28 @@
 #include <iostream>
+#include <stdlib.h>     //srand, rand
+#include <time.h>       //time
 using namespace std;
 
-int main ()
+
+int randArray [10]; //declare empty array which will hold the random numbers
+
+int main()
 {
-    for j = 2 to A.length
+    srand (time(nullptr));  //initialize random seed
+
+    int randArrayInt; //will hold generated random number
+
+    cout << "Array will hold " << 10 << " integers. \n" << endl;
+
+    cout << "[ ";
+    for (int i = 0; i < 10; i++)
+    {
+        randArrayInt = rand() % 10 + 1; // generate random number for the array
+        randArray[i] = randArrayInt;   // insert into array position the generated random number
+        cout << randArray[i] << ", ";  // print out array element at current loop position
+    }
+    cout << "] ";
+    //for j = 2 to A.length
 
     return 0;
 }
